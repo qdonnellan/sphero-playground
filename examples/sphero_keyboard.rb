f866ded87e3b10faa7a83341c02bc9e78d9e2237
qdonnellan <qdonnellan@gmail.com>
2014-06-03 14:41:16 -0500

    added an example of a keyboard controlled sphero (simple)

---
+++ examples/sphero_keyboard.rb
@@ -0,0 +1,15 @@
+require 'artoo'
+
+connection :sphero, :adaptor => :sphero, :port => '0.0.0.0:8023'
+device :sphero, :driver => :sphero
+
+connection :keyboard, :adaptor => :keyboard
+device :keyboard, :driver => :keyboard, :connection => :keyboard
+
+work do
+    on keyboard, key: :keypress
+end
+
+def keypress(sender, the_key)
+    sphero.set_color(rand(255),rand(255),rand(255))
+end
