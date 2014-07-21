960c1e97db9ca3185a07c69956340b2be7a04c7c
qdonnellan <quentin@burkesoftware.com>
2014-07-21 14:51:45 -0500

    changed the simple colors one a bit

--- examples/colors.rb
+++ examples/colors.rb
@@ -5,7 +5,17 @@ device :sphero, :driver => :sphero
 
 work do
 
-    sphero.set_color(255,0,0)
+    sphero.set_color(255, 0, 0)
     sleep 2
+
+    sphero.set_color(0, 255, 0)
+    sleep 3
+
+    sphero.set_color(0, 0, 255)
+    sleep 5
+
+    sphero.set_color(0, 0, 0)
+
+
 
 end
