4fa1de8a7d389c169953a7da82a5338732a941be
qdonnellan <quentin@burkesoftware.com>
2014-07-24 15:43:53 -0500

    whoops!

--- examples/back_led.rb
+++ examples/back_led.rb
@@ -6,12 +6,12 @@ device :sphero, :driver => :sphero
 work do
 
     sphero.set_color(255,0 , 255 )
-    sphero.back_led_output = 100
+    sphero.connection.back_led_output = 100
     sleep 3
 
 
     sphero.set_color(0,0 , 255 )
-    sphero.back_led_output = 50
+    sphero.connection.back_led_output = 50
     sleep 3
 
 
